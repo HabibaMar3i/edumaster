11b4513c2547d28304608148204eccc11b7827db
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-12-13 19:09:48 +0200

    made different layout for admin

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,8 +1,22 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import AdminSidebar from '../components/AdminSidebar'
 
 export default function MainLayout() {
+    const role = localStorage.getItem('role')
+    
+    if (role === 'admin') {
+        return (
+            <div className="flex">
+                <AdminSidebar />
+                <div className="flex-1">
+                    <Outlet />
+                </div>
+            </div>
+        )
+    }
+    
     return (
         <div>
             <Navbar />
