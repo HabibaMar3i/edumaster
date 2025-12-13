9c93ee1c05cd58a19211d47a461acb6de820067b
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-12-13 17:51:13 +0200

    added redux toolkit setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import '@fortawesome/fontawesome-free/css/all.css'
 import './index.css'
 import App from './App.jsx'
-import {HeroUIProvider} from '@heroui/react'
+import { HeroUIProvider } from '@heroui/react'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HeroUIProvider>
-    <App />
-    </HeroUIProvider>
+    <Provider store={store}>
+      <HeroUIProvider>
+        <App />
+      </HeroUIProvider>
+    </Provider>
   </StrictMode>,
 )
