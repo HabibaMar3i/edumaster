2e76194d9eb51383ca4566d151a3ff4bb913757b
basta1297 <ahmed.b.01.12.97@gmail.com>
2025-12-24 23:54:49 +0200

    ...

--- src/store.js
+++ src/store.js
@@ -3,14 +3,14 @@ import authReducer from "./features/auth/slice/authSlice";
 import examsReducer from "./features/exams/slice/examSlice.js";
 import questionsReducer from "./features/questions/slice/getAllQuestionSlice";
 import lessonReducer from "./features/Lessons/slice/lessonSlice";
-import userSlice from "./features/user/slice/userSlice.js";
+import userReducer from "./features/user/slice/userSlice.js";
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    exams: examsReducer,
-    questions: questionsReducer,
-    lessons: lessonReducer,
-    user:userSlice,
-  },
+    reducer: {
+        auth: authReducer,
+        exams: examsReducer,
+        questions: questionsReducer,
+        lessons: lessonReducer,
+        user: userReducer,
+    },
 });
